7fbe70cd5533c638fbf33614745b9107b0885a9d
Ariana Faraday <arianaloreleifaraday@gmail.com>
2022-03-05 14:56:20 -0500

    get minion by id

--- server/minions.js
+++ server/minions.js
@@ -1,18 +1,21 @@
 const express = require('express');
+const { min } = require('mocha/lib/reporters');
 const minionsRouter = express.Router();
 const { getAllFromDatabase, getFromDatabaseById, addToDatabase, updateInstanceInDatabase, deleteFromDatabasebyId } = require('./db');
+
+const type = 'minions';
 
 
 // Get all minions
 minionsRouter.get('/', (req, res, next) => {
-    res.send(getAllFromDatabase('minions'));
+    res.send(getAllFromDatabase(type));
 })
 
 // Create new minion
 minionsRouter.post('/', (req, res, next) => {
     req.body.salary = Number(req.body.salary);
     if (req.body.name && req.body.title && req.body.salary) {
-        const newMinion = addToDatabase('minions', req.body);
+        const newMinion = addToDatabase(type, req.body);
         res.status(201).send(newMinion);
     } else {
         res.status(400).send('Invalid request');
@@ -20,7 +23,14 @@ minionsRouter.post('/', (req, res, next) => {
 })
 
 // Get minion by ID
-minionsRouter.get('/:minionId', (req, res, next) => {})
+minionsRouter.get('/:minionId', (req, res, next) => {
+    const minion = getFromDatabaseById(type, req.params.minionId);
+    if (minion) {
+        res.send(minion);
+    } else {
+        res.status(404).send('Minion does not exist');
+    }
+})
 
 // Updated minion by ID
 minionsRouter.put('/:minionId', (req, res, next) => {})
